5dcfe4a7cc45be0c9b659828b006471b03193a45
jllen <jpa1408@hotmail.com>
2026-02-26 10:40:24 +0000

    Ensure Default pricing strategy

---
+++ Supermarket.Test/PricingStrategy/DefaultPricingStrategyTests.cs
@@ -0,0 +1,19 @@
+﻿using Supermarket.PricingStrategy;
+
+namespace Supermarket.Test.PricingStrategy
+{
+    public class DefaultPricingStrategyTests
+    {
+        [Fact]
+        public void TestPrice()
+        {
+            // Arrange
+            var unitPrice = 50;
+            var subject = new DefaultPricingStrategy(unitPrice);
+            // Act
+            var price = subject.Price(3);
+            // Assert
+            Assert.Equal(150, price);
+        }
+    }
+}
